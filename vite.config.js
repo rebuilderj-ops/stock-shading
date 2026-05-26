d0cff32367f924eab45a0020d22daab025b52c2d
Jaemin Yoo <jaeminyoo@waywaystudio.com>
2026-05-26 17:58:14 +0900

    chore: 카카오톡 및 모바일 웹뷰 호환성을 위해 빌드 타겟을 es2015로 하향 보정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 최신 자바스크립트 문법을 옛날 스마트폰이나 카카오톡 웹뷰에서도 안전하게 해석할 수 있도록 표준 구형 문법(es2015)으로 변환 빌드합니다.
+    target: 'es2015',
+    cssTarget: 'chrome61'
+  }
 })
